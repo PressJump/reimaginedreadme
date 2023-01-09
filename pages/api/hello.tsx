bb16af128344e68e08d7f138c4a5c4c742655ef7
PressJump <40802367+PressJump@users.noreply.github.com>
2023-01-10 00:12:42 +1100

    Return formatted usercommits stats

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,6 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 const { graphql } = require('@octokit/graphql')
 
+type UserData = {
+	thisyear: number
+	thismonth: number
+	thisweek: number
+}
+
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
@@ -30,4 +36,17 @@ export default async function handler(
 			authorization: `token ${process.env.GITHUB_TOKEN}`,
 		},
 	})
+
+	const UserData: UserData = {
+		thisyear:
+			resp.user.contributionsCollection.contributionCalendar.totalContributions,
+		thismonth: resp.user.contributionsCollection.contributionCalendar.weeks[
+			resp.user.contributionsCollection.contributionCalendar.weeks.length - 1
+		].contributionDays.reduce((a, b) => a + b.contributionCount, 0),
+		thisweek: resp.user.contributionsCollection.contributionCalendar.weeks[
+			resp.user.contributionsCollection.contributionCalendar.weeks.length - 1
+		].contributionDays.reduce((a, b) => a + b.contributionCount, 0),
+	}
+
+	return res.status(200).json(UserData)
 }
