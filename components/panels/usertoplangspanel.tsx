1c30c6ffb2d28cf72f1a64e602916d7cea0a7236
PressJump <40802367+PressJump@users.noreply.github.com>
2023-01-10 12:14:25 +1100

    Add Language Icons

--- components/panels/usertoplangspanel.tsx
+++ components/panels/usertoplangspanel.tsx
@@ -1,5 +1,14 @@
 import Head from 'next/head'
 
+type MapType = {
+	[key: string]: string
+}
+
+const whitelist: MapType = {
+	html: 'html5',
+	css: 'css3',
+}
+
 export const Usertoplangspanel = ({ topLanguages }: { topLanguages: JSON }) => {
 	return (
 		<>
@@ -20,26 +29,91 @@ export const Usertoplangspanel = ({ topLanguages }: { topLanguages: JSON }) => {
 			<text xmlns="http://www.w3.org/2000/svg" className="title" x="370" y="35">
 				Top Languages
 			</text>
-			<g id="toplangfirst" className="item" transform="translate(25, 35)">
-				<text className="contribution-stats" x="370" y="35">
-					{topLanguages[0][0]}
-				</text>
-			</g>
-			<g id="toplangfirst" className="item" transform="translate(25, 35)">
-				<text className="contribution-stats" x="370" y="70">
-					{topLanguages[1][0]}
-				</text>
-			</g>
-			<g id="toplangfirst" className="item" transform="translate(25, 35)">
-				<text className="contribution-stats" x="370" y="105">
-					{topLanguages[2][0]}
-				</text>
-			</g>
-			<g id="toplangfirst" className="item" transform="translate(25, 35)">
-				<text className="contribution-stats" x="370" y="140">
-					{topLanguages[3][0]}
-				</text>
-			</g>
+
+			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="35">
+				<g className="item" transform="translate(3, 2)">
+					<image
+						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
+							whitelist[topLanguages[0][0].toLowerCase()] ||
+							topLanguages[0][0].toLowerCase()
+						}/${
+							(whitelist[topLanguages[0][0].toLowerCase()] ||
+								topLanguages[0][0].toLowerCase()) + '-original.svg'
+						}`}
+						width="27"
+						height="27"
+						y="15"
+					/>
+				</g>
+				<g id="toplangfirst" className="item" transform="translate(25, 35)">
+					<text className="contribution-stats" x="15" y="0">
+						{topLanguages[0][0]}
+					</text>
+				</g>
+			</svg>
+			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="70">
+				<g className="item" transform="translate(3, 2)">
+					<image
+						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
+							whitelist[topLanguages[1][0].toLowerCase()] ||
+							topLanguages[1][0].toLowerCase()
+						}/${
+							(whitelist[topLanguages[1][0].toLowerCase()] ||
+								topLanguages[1][0].toLowerCase()) + '-original.svg'
+						}`}
+						width="27"
+						height="27"
+						y="15"
+					/>
+				</g>
+				<g id="toplangfirst" className="item" transform="translate(25, 35)">
+					<text className="contribution-stats" x="15" y="0">
+						{topLanguages[1][0]}
+					</text>
+				</g>
+			</svg>
+			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="105">
+				<g className="item" transform="translate(3, 2)">
+					<image
+						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
+							whitelist[topLanguages[2][0].toLowerCase()] ||
+							topLanguages[2][0].toLowerCase()
+						}/${
+							(whitelist[topLanguages[2][0].toLowerCase()] ||
+								topLanguages[2][0].toLowerCase()) + '-original.svg'
+						}`}
+						width="27"
+						height="27"
+						y="15"
+					/>
+				</g>
+				<g id="toplangfirst" className="item" transform="translate(25, 35)">
+					<text className="contribution-stats" x="15" y="0">
+						{topLanguages[2][0]}
+					</text>
+				</g>
+			</svg>
+			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="140">
+				<g className="item" transform="translate(3, 2)">
+					<image
+						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
+							whitelist[topLanguages[3][0].toLowerCase()] ||
+							topLanguages[3][0].toLowerCase()
+						}/${
+							(whitelist[topLanguages[3][0].toLowerCase()] ||
+								topLanguages[3][0].toLowerCase()) + '-original.svg'
+						}`}
+						width="27"
+						height="27"
+						y="15"
+					/>
+				</g>
+				<g id="toplangfirst" className="item" transform="translate(25, 35)">
+					<text className="contribution-stats" x="15" y="0">
+						{topLanguages[3][0]}
+					</text>
+				</g>
+			</svg>
 		</>
 	)
 }
