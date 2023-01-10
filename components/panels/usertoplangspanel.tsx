d6109bbc95a88a1402c2854fc546747252704bb5
PressJump <40802367+PressJump@users.noreply.github.com>
2023-01-10 13:29:15 +1100

    Dynamic Language List

--- components/panels/usertoplangspanel.tsx
+++ components/panels/usertoplangspanel.tsx
@@ -35,90 +35,35 @@ export const Usertoplangspanel = ({
 				Top Languages
 			</text>
 
-			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="35">
-				<g className="item" transform="translate(3, 2)">
-					<image
-						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
-							whitelist[topLanguages[0][0].toLowerCase()] ||
-							topLanguages[0][0].toLowerCase()
-						}/${
-							(whitelist[topLanguages[0][0].toLowerCase()] ||
-								topLanguages[0][0].toLowerCase()) + '-original.svg'
-						}`}
-						width="27"
-						height="27"
-						y="15"
-					/>
-				</g>
-				<g id="toplangfirst" className="item" transform="translate(25, 35)">
-					<text className="contribution-stats" x="15" y="0">
-						{topLanguages[0][0]}
-					</text>
-				</g>
-			</svg>
-			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="70">
-				<g className="item" transform="translate(3, 2)">
-					<image
-						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
-							whitelist[topLanguages[1][0].toLowerCase()] ||
-							topLanguages[1][0].toLowerCase()
-						}/${
-							(whitelist[topLanguages[1][0].toLowerCase()] ||
-								topLanguages[1][0].toLowerCase()) + '-original.svg'
-						}`}
-						width="27"
-						height="27"
-						y="15"
-					/>
-				</g>
-				<g id="toplangfirst" className="item" transform="translate(25, 35)">
-					<text className="contribution-stats" x="15" y="0">
-						{topLanguages[1][0]}
-					</text>
-				</g>
-			</svg>
-			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="105">
-				<g className="item" transform="translate(3, 2)">
-					<image
-						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
-							whitelist[topLanguages[2][0].toLowerCase()] ||
-							topLanguages[2][0].toLowerCase()
-						}/${
-							(whitelist[topLanguages[2][0].toLowerCase()] ||
-								topLanguages[2][0].toLowerCase()) + '-original.svg'
-						}`}
-						width="27"
-						height="27"
-						y="15"
-					/>
-				</g>
-				<g id="toplangfirst" className="item" transform="translate(25, 35)">
-					<text className="contribution-stats" x="15" y="0">
-						{topLanguages[2][0]}
-					</text>
-				</g>
-			</svg>
-			<svg xmlns="http://www.w3.org/2000/svg" x="370" y="140">
-				<g className="item" transform="translate(3, 2)">
-					<image
-						href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
-							whitelist[topLanguages[3][0].toLowerCase()] ||
-							topLanguages[3][0].toLowerCase()
-						}/${
-							(whitelist[topLanguages[3][0].toLowerCase()] ||
-								topLanguages[3][0].toLowerCase()) + '-original.svg'
-						}`}
-						width="27"
-						height="27"
-						y="15"
-					/>
-				</g>
-				<g id="toplangfirst" className="item" transform="translate(25, 35)">
-					<text className="contribution-stats" x="15" y="0">
-						{topLanguages[3][0]}
-					</text>
-				</g>
-			</svg>
+			{topLanguages.map((lang, index) => {
+				return (
+					<svg
+						key={index}
+						xmlns="http://www.w3.org/2000/svg"
+						x="370"
+						y={index * 35 + 35}
+					>
+						<g className="item" transform="translate(3, 2)">
+							<image
+								href={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${
+									whitelist[lang[0].toLowerCase()] || lang[0].toLowerCase()
+								}/${
+									(whitelist[lang[0].toLowerCase()] || lang[0].toLowerCase()) +
+									'-original.svg'
+								}`}
+								width="27"
+								height="27"
+								y="15"
+							/>
+						</g>
+						<g id="toplangfirst" className="item" transform="translate(25, 35)">
+							<text className="contribution-stats" x="15" y="0">
+								{lang[0]}
+							</text>
+						</g>
+					</svg>
+				)
+			})}
 		</>
 	)
 }
