054dc65fb3539518801496e2d67e61e80ee186cc
PressJump <40802367+PressJump@users.noreply.github.com>
2023-01-13 18:31:05 +1100

    ➕ Add TailwindCSS & Init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+	content: ['./pages/**/*.{ts,tsx}', './public/**/*.html'],
+	plugins: [],
+	theme: {},
+	//dark mode only class
+	darkMode: 'class',
+}
